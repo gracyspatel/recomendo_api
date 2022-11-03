29d5ea5ee43e23c151348ffe809f7d704659bb4d
gracyspatel <gracyspatel@outlook.com>
2022-11-03 13:05:39 +0530

    book api

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ import numpy as np
 from collections import defaultdict
 
 
+def recommend_book(book_name):
+    pt = joblib.load("./Pickle/Books/model_pt.pkl")
+    similarity_score = joblib.load("./Pickle/Books/model_simscore.pkl")
+    index = np.where(pt.index==book_name)[0][0]
+    similar_items = sorted(list(enumerate(similarity_score[index])),key= lambda x:x[1],reverse = True)[1:11]
+    booklist = []
+    for i in similar_items:
+        booklist.append(pt.index[i[0]])
+    return booklist
+
+
 
 app =  Flask(__name__)
 
@@ -35,3 +46,28 @@ def predict_songs():
 
             return jsonify({'trace': traceback.format_exc()})
 
+
+# API endpint /movie to predict movies based on credits and keywords and genres
+@app.route('/book', methods=['POST'])
+def predict_book():
+        try:
+            content_type = request.headers.get('Content-Type')
+            if (content_type == 'application/json'):
+                json = request.json
+            book_name = json['book']
+            return jsonify({"book_name": book_name},{"recommendation":recommend_book(book_name)})
+
+        except:
+
+            return jsonify({'trace': traceback.format_exc()})
+
+# API endpint /movie to predict movies based on credits and keywords and genres
+@app.route('/topbook', methods=['GET'])
+def top_book():
+        try:
+            topbooks = joblib.load("./Pickle/Books/model_topbooks.pkl")
+            return topbooks.to_json(orient='table')
+
+        except:
+
+            return jsonify({'trace': traceback.format_exc()})
